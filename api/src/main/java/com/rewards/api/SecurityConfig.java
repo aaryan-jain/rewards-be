c9d733134477f311c7ea4e142dbbaac11e434afd
AARYANS-POWERHO\Aaryan Jain <aaryan.vijayraj@gmail.com>
2024-03-09 12:59:02 +0530

    added authorisation to swagger docs

--- api/src/main/java/com/rewards/api/SecurityConfig.java
+++ api/src/main/java/com/rewards/api/SecurityConfig.java
@@ -2,6 +2,13 @@ package com.rewards.api;
 
 import com.rewards.api.auth.JwtAuthEntryPoint;
 import com.rewards.api.auth.JwtAuthenticationFilter;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,5 +36,25 @@ public class SecurityConfig {
                 .and().exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthEntryPoint));
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("Aaryan Jain")
+                                .email("aaryan.vijayraj@gmail.com").url("aaryan.vijayraj@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
     }
 }
